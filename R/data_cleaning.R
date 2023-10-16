fba4a6d0ad683213376d13dd5e0ea06cec9c8745
Ted Hermann <ted.w.hermann@gmail.com>
2023-10-16 13:35:15 +0200

    moving everything towards targets

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -5,55 +5,60 @@ library(tidymodels)
 
 
 
-data_folder <- here::here("data", "csv", "iep")
-files <- list.files(data_folder)
+# data_folder <- here::here("data", "csv", "iep")
+# files <- list.files(data_folder)
 
 
 get_csv_from_path <- function(file, folder) {
-  path <- here(data_folder, file)
+  path <- here(folder, file)
   read_csv(path, show_col_types = FALSE)
 }
 
-file_list <- map(files, get_csv_from_path, folder = data_folder)
+# file_list <- map(files, get_csv_from_path, folder = data_folder)
 
-tax_cols <- file_list[[2]] %>% names()
-names_to_drop <- tax_cols[tax_cols != "CommonName"]
+# tax_cols <- file_list[[2]] %>% names()
+# names_to_drop <- tax_cols[tax_cols != "CommonName"]
 
-full_data <- left_join(file_list[[1]], file_list[[2]], by = "OrganismCode") %>%
-  select(-all_of(names_to_drop), -OrganismCode)
+get_full_data_set <- function(file_list, names_to_drop){
 
-lmb <- full_data %>%
-  rename_all(tolower) %>%
-  mutate(across(c(segment_number, weathercode, channel_type, dominant_substrate, tide, bank_type), as_factor)) %>%
-  filter(!is.na(seconds_per_transect) | !is.na(seconds_per_segment) | !is.na(commonname)) %>%
-  # mutate(scaled_time = seconds_per_transect / min(seconds_per_transect),
-  #        scaled_count = count / scaled_time) %>%
-  group_by(regioncode, subregion, sampledate, starttime, segment_number, commonname) %>%
-  mutate(
-    segment_species_count = sum(count),
-    lmb = if_else(commonname == "largemouth bass", segment_species_count, 0)
-  ) %>%
-  ungroup() %>%
-  select(-c(
-    stoptime,
-    depth_sampled,
-    estimated_height,
-    sampling_direction:research_vessel,
-      pulse_per_second:peak_watts,
-    markcode:segment_species_count,
-    seconds_per_segment:gps_end_longitude,
-    specific_conductance
-  )) %>%
-  distinct() %>%
-  drop_na() %>%
-  mutate(across(emergent:bare, ~ .x / 100)) %>%
-  select(-bare) %>%
-  group_by(regioncode, subregion, sampledate, starttime, segment_number) %>%
-  mutate(sum_lmb = sum(lmb)) %>%
-  ungroup() %>%
-  select(-lmb) %>%
-  distinct()
+  left_join(file_list[[1]], file_list[[2]], by = "OrganismCode") %>%
+    select(-all_of(names_to_drop), -OrganismCode)
+}
 
+get_lmb_data <- function(full_data){
+
+  full_data %>%
+    rename_all(tolower) %>%
+    mutate(across(c(segment_number, weathercode, channel_type, dominant_substrate, tide, bank_type), as_factor)) %>%
+    filter(!is.na(seconds_per_transect) | !is.na(seconds_per_segment) | !is.na(commonname)) %>%
+    # mutate(scaled_time = seconds_per_transect / min(seconds_per_transect),
+    #        scaled_count = count / scaled_time) %>%
+    group_by(regioncode, subregion, sampledate, starttime, segment_number, commonname) %>%
+    mutate(
+      segment_species_count = sum(count),
+      lmb = if_else(commonname == "largemouth bass", segment_species_count, 0)
+    ) %>%
+    ungroup() %>%
+    select(-c(
+      stoptime,
+      depth_sampled,
+      estimated_height,
+      sampling_direction:research_vessel,
+        pulse_per_second:peak_watts,
+      markcode:segment_species_count,
+      seconds_per_segment:gps_end_longitude,
+      specific_conductance
+    )) %>%
+    distinct() %>%
+    drop_na() %>%
+    mutate(across(emergent:bare, ~ .x / 100)) %>%
+    select(-bare) %>%
+    group_by(regioncode, subregion, sampledate, starttime, segment_number) %>%
+    mutate(sum_lmb = sum(lmb)) %>%
+    ungroup() %>%
+    select(-lmb) %>%
+    distinct()
+}
 find_mode <- function(x) {
   u <- unique(x)
   tab <- tabulate(match(x, u))
@@ -68,142 +73,142 @@ find_mode <- function(x) {
 #             across(.cols = where(is.numeric), .fns = mean),
 #             .groups = "drop")
 
-lmb_fitting <- lmb %>%
-  select(-c(regioncode:starttime, segment_number))
+# lmb_fitting <- lmb %>%
+#   select(-c(regioncode:starttime, segment_number))
 
-set.seed(123)
-split <- initial_split(lmb_fitting)
-train <- training(split)
-test <- testing(split)
-
-
-lm_spec <- linear_reg() %>%
-  set_engine("lm")
-
-lm_fit <- lm_spec %>%
-  fit(sum_lmb ~ ., data = train)
+# set.seed(123)
+# split <- initial_split(lmb_fitting)
+# train <- training(split)
+# test <- testing(split)
 
 
-rf_spec <- rand_forest(mode = "regression") %>%
-  set_engine("ranger")
+# lm_spec <- linear_reg() %>%
+#   set_engine("lm")
 
-rf_fit <- rf_spec %>%
-  fit(sum_lmb ~ ., data = train)
-
-rf_fit
+# lm_fit <- lm_spec %>%
+#   fit(sum_lmb ~ ., data = train)
 
 
-results_train <- lm_fit %>%
-  predict(new_data = train) %>%
-  mutate(
-    truth = train$sum_lmb,
-    model = "lm"
-  ) %>%
-  bind_rows(rf_fit %>%
-    predict(new_data = train) %>%
-    mutate(
-      truth = train$sum_lmb,
-      model = "rf"
-    ))
+# rf_spec <- rand_forest(mode = "regression") %>%
+#   set_engine("ranger")
 
-results_test <- lm_fit %>%
-  predict(new_data = test) %>%
-  mutate(
-    truth = test$sum_lmb,
-    model = "lm"
-  ) %>%
-  bind_rows(rf_fit %>%
-    predict(new_data = test) %>%
-    mutate(
-      truth = test$sum_lmb,
-      model = "rf"
-    ))
+# rf_fit <- rf_spec %>%
+#   fit(sum_lmb ~ ., data = train)
 
-results_train %>%
-  group_by(model) %>%
-  rmse(truth = truth, estimate = .pred)
-
-results_test %>%
-  group_by(model) %>%
-  rmse(truth = truth, estimate = .pred)
+# rf_fit
 
 
-folds <- vfold_cv(train)
-wf <- workflow() %>%
-  add_model(rf_spec)
+# results_train <- lm_fit %>%
+#   predict(new_data = train) %>%
+#   mutate(
+#     truth = train$sum_lmb,
+#     model = "lm"
+#   ) %>%
+#   bind_rows(rf_fit %>%
+#     predict(new_data = train) %>%
+#     mutate(
+#       truth = train$sum_lmb,
+#       model = "rf"
+#     ))
 
-rf_res <- fit_resamples(
-  workflow(sum_lmb ~ ., rf_spec),
-  folds,
-  control = control_resamples(save_pred = TRUE)
-)
+# results_test <- lm_fit %>%
+#   predict(new_data = test) %>%
+#   mutate(
+#     truth = test$sum_lmb,
+#     model = "lm"
+#   ) %>%
+#   bind_rows(rf_fit %>%
+#     predict(new_data = test) %>%
+#     mutate(
+#       truth = test$sum_lmb,
+#       model = "rf"
+#     ))
 
-rf_res %>%
-  collect_metrics()
+# results_train %>%
+#   group_by(model) %>%
+#   rmse(truth = truth, estimate = .pred)
 
-rf_res %>%
-  unnest(.predictions) %>%
-  ggplot(aes(sum_lmb, .pred, color = id)) +
-  geom_abline(lty = 2, color = "gray80", size = 1.5) +
-  geom_point(alpha = 0.5) +
-  labs(
-    x = "Truth",
-    y = "Predicted value",
-    color = NULL
-  ) +
-  theme_bw()
-
-
-results_test %>%
-  mutate(train = "testing") %>%
-  bind_rows(results_train %>%
-    mutate(train = "training")) %>%
-  ggplot(aes(truth, .pred, color = model)) +
-  geom_abline(lty = 2, color = "gray80", size = 1.5) +
-  geom_point(alpha = 0.5) +
-  facet_wrap(~train) +
-  labs(
-    x = "Truth",
-    y = "Predicted value",
-    color = "Type of model"
-  ) +
-  xlim(0, 20) +
-  ylim(0, 20) +
-  theme_bw()
+# results_test %>%
+#   group_by(model) %>%
+#   rmse(truth = truth, estimate = .pred)
 
 
-results_train %>% 
-  filter(model == "lm") %>% 
-  mutate(residual = truth - .pred) %>% 
-  ggplot(aes(x = truth, y = residual)) +
-  # geom_abline(lty = 2, color = "black", size = 1.5) +
-  geom_point(alpha = 0.5, color = "dodgerblue") + 
-  theme_bw() +
-  xlim(0,25) +
-  ylim(0,25) +
-  xlab("observed number of fish") +
-  ylab("residual of prediction")
+# folds <- vfold_cv(train)
+# wf <- workflow() %>%
+#   add_model(rf_spec)
+
+# rf_res <- fit_resamples(
+#   workflow(sum_lmb ~ ., rf_spec),
+#   folds,
+#   control = control_resamples(save_pred = TRUE)
+# )
+
+# rf_res %>%
+#   collect_metrics()
+
+# rf_res %>%
+#   unnest(.predictions) %>%
+#   ggplot(aes(sum_lmb, .pred, color = id)) +
+#   geom_abline(lty = 2, color = "gray80", size = 1.5) +
+#   geom_point(alpha = 0.5) +
+#   labs(
+#     x = "Truth",
+#     y = "Predicted value",
+#     color = NULL
+#   ) +
+#   theme_bw()
 
 
-m <- full_data %>%
-  rename_all(tolower) %>%
-  filter(!is.na(seconds_per_transect)) %>%
-  group_by(sampledate, starttime, segment_number, commonname, seconds_per_transect) %>%
-  mutate(sum = sum(count)) %>%
-  ungroup() %>%
-  select(sampledate, starttime, segment_number, commonname, seconds_per_transect, sum) %>%
-  drop_na() %>%
-  distinct()
+# results_test %>%
+#   mutate(train = "testing") %>%
+#   bind_rows(results_train %>%
+#     mutate(train = "training")) %>%
+#   ggplot(aes(truth, .pred, color = model)) +
+#   geom_abline(lty = 2, color = "gray80", size = 1.5) +
+#   geom_point(alpha = 0.5) +
+#   facet_wrap(~train) +
+#   labs(
+#     x = "Truth",
+#     y = "Predicted value",
+#     color = "Type of model"
+#   ) +
+#   xlim(0, 20) +
+#   ylim(0, 20) +
+#   theme_bw()
 
-model <- lm(sum ~ seconds_per_transect, data = m)
 
-m2 <- m %>% mutate(pred = predict(model))
+# results_train %>% 
+#   filter(model == "lm") %>% 
+#   mutate(residual = truth - .pred) %>% 
+#   ggplot(aes(x = truth, y = residual)) +
+#   # geom_abline(lty = 2, color = "black", size = 1.5) +
+#   geom_point(alpha = 0.5, color = "dodgerblue") + 
+#   theme_bw() +
+#   xlim(0,25) +
+#   ylim(0,25) +
+#   xlab("observed number of fish") +
+#   ylab("residual of prediction")
 
 
-ggplot(data = m, aes(x = seconds_per_transect, y = sum)) +
-  geom_point(alpha = 0.5) +
-  geom_line(data = m2, aes(x = seconds_per_transect, y = pred), color = "red") +
-  theme_bw()
+# m <- full_data %>%
+#   rename_all(tolower) %>%
+#   filter(!is.na(seconds_per_transect)) %>%
+#   group_by(sampledate, starttime, segment_number, commonname, seconds_per_transect) %>%
+#   mutate(sum = sum(count)) %>%
+#   ungroup() %>%
+#   select(sampledate, starttime, segment_number, commonname, seconds_per_transect, sum) %>%
+#   drop_na() %>%
+#   distinct()
+
+# model <- lm(sum ~ seconds_per_transect, data = m)
+
+# m2 <- m %>% mutate(pred = predict(model))
+
+
+# ggplot(data = m, aes(x = seconds_per_transect, y = sum)) +
+#   geom_point(alpha = 0.5) +
+#   geom_line(data = m2, aes(x = seconds_per_transect, y = pred), color = "red") +
+#   theme_bw()
 
 # data_folder <- here("data", "res_shore_fish", "csv")
 #
---
+++ _targets.R
@@ -0,0 +1,97 @@
+# Created by use_targets().
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
+
+# Load packages required to define the pipeline:
+library(targets)
+library(here)
+# library(tarchetypes) # Load other packages as needed.
+
+# Set target options:
+tar_option_set(
+  packages = c(
+    "tidyverse",
+    "tidymodels",
+    "broom",
+    "bonsai"
+  ) # packages that your targets need to run
+  # format = "qs", # Optionally set the default storage format. qs is fast.
+  #
+  # For distributed computing in tar_make(), supply a {crew} controller
+  # as discussed at https://books.ropensci.org/targets/crew.html.
+  # Choose a controller that suits your needs. For example, the following
+  # sets a controller with 2 workers which will run as local R processes:
+  #
+  #   controller = crew::crew_controller_local(workers = 2)
+  #
+  # Alternatively, if you want workers to run on a high-performance computing
+  # cluster, select a controller from the {crew.cluster} package. The following
+  # example is a controller for Sun Grid Engine (SGE).
+  # 
+  #   controller = crew.cluster::crew_controller_sge(
+  #     workers = 50,
+  #     # Many clusters install R as an environment module, and you can load it
+  #     # with the script_lines argument. To select a specific verison of R,
+  #     # you may need to include a version string, e.g. "module load R/4.3.0".
+  #     # Check with your system administrator if you are unsure.
+  #     script_lines = "module load R"
+  #   )
+  #
+  # Set other options as needed.
+)
+
+# tar_make_clustermq() is an older (pre-{crew}) way to do distributed computing
+# in {targets}, and its configuration for your machine is below.
+options(clustermq.scheduler = "multicore")
+
+# tar_make_future() is an older (pre-{crew}) way to do distributed computing
+# in {targets}, and its configuration for your machine is below.
+# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
+
+# Run the R scripts in the R/ folder with your custom functions:
+# tar_source()
+source(here("R", "data_cleaning.R"))
+
+list(
+  tar_target(
+    name = data_folder,
+    command = here::here("data", "csv", "iep")
+  ),
+  tar_target(
+    name = files,
+    command = list.files(data_folder)
+  ),
+  tar_target(
+    name = file_list,
+    command = map(files, get_csv_from_path, folder = data_folder)
+  ),
+  tar_target(
+    name = tax_cols,
+    command = file_list[[2]] %>% names()
+  ),
+  tar_target(
+    name = names_to_drop,
+    command = tax_cols[tax_cols != "CommonName"]
+  ),
+  tar_target(
+    name = full_data,
+    command = get_full_data_set(file_list, names_to_drop)
+  ),
+  tar_target(
+    name = lmb_data,
+    command = get_lmb_data(full_data)
+  )
+
+  # tar_target(
+  #   name = data_folder,
+  #   command = here::here("data", "csv", "iep")
+  #   # format = "feather" # efficient storage for large data frames
+  # ),
+  # tar_target(
+  #   name = data_folder,
+  #   command = here::here("data", "csv", "iep")
+  #   # format = "feather" # efficient storage for large data frames
+  # ),
+
+)
